987abf94004c74064b46293da705bb4f77b06101
Rogerio Siqueira de Miranda <juka_jms@hotmail.com>
2022-07-05 09:28:01 -0300

    Projeto Imob

---
+++ backend/src/test/java/com/imob/jImov/JImovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imob.jImov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JImovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
